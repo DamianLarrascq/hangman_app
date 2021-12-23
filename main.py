38d08432dd8d3edf18f4779526db15508fd7989d
Damian <damian.larrascq@gmail.com>
2021-12-23 08:33:27 -0300

    added hangman func

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import random
-from words import words
+from words import words as mywords
+import string
+
 
 def get_valid_word(words):
-    word = random.choice(words)
-    while '-' in words or ' ' in words:
-        word = random.choice(words)
+    word = random.choice(words)     #chooses random letter from words file
+    while '-' in words or ' ' in words:     #iterates through words until it finds a word without dash or space
+        word = random.choice(words)     #inserts word from file to variable word
 
-    return word.upper()
+    return word.upper()     #returns the word in uppercase
+
+
+def hangman():
+    word_sel = get_valid_word(mywords)      #stores the chosen word
+    word_letters = set(word_sel)        #divides word into separate letters
+    alphabet = set(string.ascii_uppercase)  #english alphabet
+    used_letters = set()        #variable storing the letters already used by the player
+    user_input = input('Type letter: ').upper()
+
+    if user_input in alphabet - used_letters:
+        used_letters.add(user_input)
+        if user_input in word_letters:
+            word_letters.remove(user_input)
+
+    elif user_input in used_letters:
+        print('Letter already used')
+
+    else:
+        print('Invalid character')
+
 
